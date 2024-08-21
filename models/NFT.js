81cab9b10408609550ea51a013041c9ad02c32b7
becky449 <rabnab12@gmail.com>
2024-08-21 17:47:43 +0300

    Data is saved on db and retrieved from db to the user via tokenid

--- models/NFT.js
+++ models/NFT.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
 const nftSchema = new mongoose.Schema({
-    tokenId: { type: Number, required: true, unique: true },
-    name: { type: String, required: true },
-    description: { type: String },
-    image: { type: String },
+  tokenId: { type: String, required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
 });
 
-const NFT = mongoose.model('NFT', nftSchema);
-
-module.exports = NFT;
+module.exports = mongoose.model('NFT', nftSchema);
